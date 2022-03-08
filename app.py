c4c2e1cd287bef37b7b3d86bb776a2234b6f20ca
Shahla waheed <shehla.waheed84@gmail.com>
2022-03-08 16:59:05 +0000

    deleted sensitive data in our list

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import csv
+from pprint import pp
 
 def extract_data_from_csv():
     sales_data = []
     with open('chesterfield_25-08-2021_09-00-00.csv','r') as file:
-        fields_names = ['order_date_time', 'branch_location', 'customer_name', 'order', 'total_payment', 'payment_type', 'card_number']
-        reader = csv.DictReader(file, fields_names)
+        # fields_names = ['order_date_time', 'branch_location',  'order_items', 'total_payment', 'payment_type']
+        reader = csv.reader(file)
 
         for row in reader:
-            sales_data.append(row)
+            sales_list = [row[0],row[1],row[3].split(','),row[4],row[5]]
+            sales_data.append(sales_list)
 
-    print(sales_data)
 
-extract_data_from_csv()
+    return sales_data
+pp(extract_data_from_csv())
