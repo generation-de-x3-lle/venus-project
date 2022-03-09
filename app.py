102404bee1cdf071f49174f0044ea304b65ea51b
Shahla waheed <shehla.waheed84@gmail.com>
2022-03-09 11:43:14 +0000

    added a function to remove data

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import csv
 from pprint import pp
 
-def extract_data_from_csv():
+def extract_raw_data_from_csv():
     sales_data = []
-    with open('chesterfield_25-08-2021_09-00-00.csv','r') as file:
-        # fields_names = ['order_date_time', 'branch_location',  'order_items', 'total_payment', 'payment_type']
-        reader = csv.reader(file)
 
-        for row in reader:
-            sales_list = [row[0],row[1],row[3].split(','),row[4],row[5]]
-            sales_data.append(sales_list)
+    try:
+        with open('chesterfield_25-08-2021_09-00-00.csv','r') as file:
+            field_names = ['order_date_time', 'branch_location','customer_name', 'order_items', 'total_payment', 'payment_type', 'card_number']
+            reader = csv.DictReader(file, field_names)
+            
+            raw_sales_data = []
+            for row in reader:
+                raw_sales_data.append(row)
+    except Exception as err:
+        print(f"An error occured: {str(err)}")
 
+    return raw_sales_data
 
-    return sales_data
-pp(extract_data_from_csv())
+raw_sales_data = extract_raw_data_from_csv()
