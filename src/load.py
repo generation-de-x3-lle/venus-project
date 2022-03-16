fd901ba7eb4ff0bea24ca7295e9fea3e38eff830
morphy418 <z.harshegyi@gmail.com>
2022-03-16 14:18:56 +0000

    added remove_whitespace and no_duplicate_product functions

--- src/load.py
+++ src/load.py
@@ -1,21 +1,20 @@
-from app import normalised_data
+from app import normalised_data, no_duplicate_products
 import db
+from pprint import pp
 
 def load_data_into_db(data):
-    for row in data:
-        products = row.get('order_items')
-        for product in products:
-            
-            sql = f'''
-            INSERT INTO products(product,flavour_id,price)
-            VALUES
-            ('{product['product_name']}','{product['flavour']}','{product['product_price']}')
-            ON CONFLICT ON CONSTRAINT '{product['flavour']}','{product['product_name']}'
-            DO  NOTHING
-            '''
-            db.load_data(sql)
-
-load_data_into_db(normalised_data)
+  for product in data:
+      
+      sql = f'''
+      INSERT INTO products(product,flavour_id,price)
+      VALUES
+      ('{product['product_name']}','{product['flavour']}','{product['product_price']}')
+      ON CONFLICT (product_id)
+      DO  NOTHING
+      '''
+      db.load_data(sql)
+  
+load_data_into_db(no_duplicate_products)
 
 
 
