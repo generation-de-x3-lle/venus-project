6bd7c024e53bbd94282b80cedc104b97f265b320
morphy418 <z.harshegyi@gmail.com>
2022-03-22 13:49:31 +0000

    inital changes in ETL lambda

--- src/lambda_etl.py
+++ src/lambda_etl.py
@@ -1,12 +1,20 @@
+import os
 import csv
 from pprint import pp
 from datetime import datetime
+import db
+import logging
+import boto3
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
 
-def extract_raw_data_from_csv():
+
+
+def extract_raw_data_from_csv(filename):
     raw_sales_data = []
 
     try:
-        with open('data/chesterfield_25-08-2021_09-00-00.csv','r') as file:
+        with open(f'/tmp/{filename}','r') as file:
 
             field_names = ['order_date_time', 'branch_location','customer_name', 'order_items', 'total_payment', 'payment_type', 'card_number']
             reader = csv.DictReader(file, field_names)
@@ -19,15 +27,12 @@ def extract_raw_data_from_csv():
 
     return raw_sales_data
 
-raw_sales_data = extract_raw_data_from_csv()
-
 def remove_sensitive_data(raw_data):
     for item in raw_data:
         del item['customer_name']
         del item['card_number']
     return raw_data
 
-cleaned_sales_data = remove_sensitive_data(raw_sales_data)
 
 def remove_whitespace_from_dict_values_in_list(list_of_dicts):
   clean_data = []
@@ -73,7 +78,6 @@ def normalise_data(cleaned_data):
 
   return normalised_data_list
 
-normalised_data = normalise_data(cleaned_sales_data)
 # pp(normalised_data)
 
 def no_duplicate_products(data):
@@ -90,6 +94,61 @@ def no_duplicate_products(data):
   
   return no_duplicate_products
 
-no_duplicate_products = no_duplicate_products(normalised_data)
+#LOAD.PY
 
+def load_data_into_db(product_data, order_data):
+  products_with_id_list = []
+  for product in product_data:
+    sql_product = f'''
+    INSERT INTO products(product,flavour,price)
+    VALUES
+    ('{product['product_name']}','{product['flavour']}','{product['product_price']}')
+    RETURNING product_id
+    '''
+    product_id = db.load_data(sql_product)
+    product['product_id'] = product_id
 
+    products_with_id_list.append(product) 
+
+  orders_with_id_list = []
+  for order in order_data:
+    sql_order = f'''
+    INSERT INTO orders (order_date, branch_location, total_payment, payment_type)
+    VALUES ('{order['order_date_time']}','{order['branch_location']}','{order['total_payment']}','{order['payment_type']}')
+    RETURNING order_id
+    '''
+    order_id = db.load_data(sql_order)
+    order['order_id'] = order_id
+    orders_with_id_list.append(order)
+
+    for order_item in order['order_items']:
+      for item_with_id in products_with_id_list:
+        if order_item['product_name'] == item_with_id['product_name'] and order_item['flavour'] == item_with_id['flavour']:
+          product_id = item_with_id['product_id']
+
+          sql_prods_on_order = f'''
+          INSERT INTO products_on_order (order_id, product_id)
+          VALUES ('{order_id}', '{product_id}')
+          '''
+          db.load_data(sql_prods_on_order)
+  
+  # pp(products_with_id_list)
+  return orders_with_id_list
+  
+
+def handler(event, context):
+  LOGGER.info(f'Event structure: {event}')
+
+  bucket = 'de-x3-lle-venus'
+  filename = 'chesterfield_16-03-2022_09-00-00.csv'
+
+  s3 = boto3.resource('s3')
+  s3.meta.client.download_file(bucket, f'/2022/3/16/{filename}', f'/tmp/{filename}')
+  os.chdir('/tmp')
+  
+  raw_sales_data = extract_raw_data_from_csv(filename)
+  cleaned_sales_data = remove_sensitive_data(raw_sales_data)
+  normalised_data = normalise_data(cleaned_sales_data)
+  no_duplicate_products = no_duplicate_products(normalised_data)
+
+  load_data_into_db(no_duplicate_products, cleaned_sales_data)
